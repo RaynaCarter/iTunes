4b6a6ab7b2b87cc3033b2b8631a62bdd328be019
Rayna Carter <RaynaCarter@students.berkeley.net>
2019-03-25 13:51:47 -0700

    //

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 
+function start(){
+
+    var enterName = document.getElementById('enterName').value;
+
     $.ajax({
-        url: 'https://itunes.apple.com/search?term=' + ,
+        url: 'https://itunes.apple.com/search?term=' + enterName,
+        type: 'GET',
+        crossDomain: true,
         dataType: 'jsonp',
-        success: myCallback(id="enterName")
+        success: myCallback
+        //error: function() { alert('Failed!'); }
     });
+
+}
 
 function myCallback(myData) {
     console.log(myData);
+    for(var i = 0; i < myData.results.length; i++){
+        document.getElementById("results").innerHTML += results[i];
+    }
+
 }
