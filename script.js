2ceb04f0decad10a0b41083b58e3cea82ca672a5
Rayna Carter <RaynaCarter@students.berkeley.net>
2019-03-26 13:21:16 -0700

    //

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function start(){
 
     var enterName = document.getElementById('enterName').value;
+    //var numOfRe = document.getElementById('numberOfResults').value;
 
     $.ajax({
         url: 'https://itunes.apple.com/search?term=' + enterName,
@@ -9,15 +10,20 @@ function start(){
         crossDomain: true,
         dataType: 'jsonp',
         success: myCallback
-        //error: function() { alert('Failed!'); }
+        //error: failed
     });
 
 }
 
 function myCallback(myData) {
+    var numOfRe = document.getElementById('numberOfResults').value;
     console.log(myData);
-    for(var i = 0; i < myData.results.length; i++){
-        document.getElementById("results").innerHTML += results[i];
+
+    for(var i = 0; i < numOfRe; i++){
+        document.getElementById("songs").innerHTML += "<br>" + myData.results[i].trackName ;
     }
 
+}
+function failed(){
+        document.getElementById("songs").innerHTML += "<br>" + alert(" No results found") ;
 }
