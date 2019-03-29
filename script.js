dd8b5059777478226314fd53f9bc1e7585ea73f4
Rayna Carter <RaynaCarter@students.berkeley.net>
2019-03-29 13:20:34 -0700

    //

--- script.js
+++ script.js
@@ -18,16 +18,24 @@ function start(){
 function myCallback(myData) {
     var numOfRe = document.getElementById('numberOfResults').value;
     console.log(myData);
+    var tableInfo = "";
+    var rank = 0;
+
+
 
     for(var i = 0; i < numOfRe; i++){
-        document.getElementById("songs").innerHTML += "<tr><td><br>Artist Name: " + myData.results[i].artistName+ "<br></td>";
-        document.getElementById("songs").innerHTML += "<td>"+"<br>" + "Track Name: "+ myData.results[i].trackName+ "<br>" + "</td>";
-        document.getElementById("songs").innerHTML += "<td>"+"<br>" + " Album: "+ myData.results[i].collectionName+ "<br>" + "</td>";
-        document.getElementById("songs").innerHTML += "<td>"+"<br>" + "<img src = " + myData.results[i].artworkUrl100
+        var rank = i+1;
+        tableInfo += "<tr>"+"<td>"+"<br>"+ rank + "<br></td>";
+        tableInfo  += "<td>"+"<br>" + "<img src = " + myData.results[i].artworkUrl100
             +">"+"<br>" + "</td>";
-        document.getElementById("songs").innerHTML += "<audio controls='true' src=" + myData.results[i].previewUrl + " id='audio' type='audio/m4a'></audio>"+"</tr>";
-
+        tableInfo += "<td><br>Artist Name: " + myData.results[i].artistName+ "<br></td>";
+        tableInfo  += "<td>"+"<br>" + "Track Name: "+ myData.results[i].trackName+ "<br>" + "</td>";
+        tableInfo  += "<td>"+"<br>" + " Album: "+ myData.results[i].collectionName+ "<br>" + "</td>";
+        tableInfo  += "<td><audio controls='true' src=" + myData.results[i].previewUrl + " id='audio' type='audio/m4a'></audio>"+"</td></tr>";
     }
+
+
+    document.getElementById("songs").innerHTML = tableInfo;
 
 }
 function failed(){
