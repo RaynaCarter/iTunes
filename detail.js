b5403953b02c39ef710c31c0312ad0c86eb0db04
Rayna Carter <RaynaCarter@students.berkeley.net>
2019-04-11 13:22:56 -0700

    //

---
+++ detail.js
@@ -0,0 +1,32 @@
+
+console.log(getQueryParameter("song"));
+console.log(getQueryParameter("artist"));
+
+
+function start(){
+
+    var enterName = document.getElementById('enterName').value;
+    //var numOfRe = document.getElementById('numberOfResults').value;
+
+    $.ajax({
+        url: 'https://itunes.apple.com/search?term=' + enterName,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: myCallback,
+        //error: failed,
+    });
+
+}
+
+//copy me into your script to get a query parameter
+function getQueryParameter(name)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == name){return pair[1];}
+    }
+    return false;
+}
--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function start(){
         type: 'GET',
         crossDomain: true,
         dataType: 'jsonp',
-        success: myCallback
-        //error: failed
+        success: myCallback,
+        //error: failed,
     });
 
 }
@@ -19,25 +19,47 @@ function myCallback(myData) {
     var numOfRe = document.getElementById('numberOfResults').value;
     console.log(myData);
     var tableInfo = "";
-    var rank = 0;
+
+    if(myData==0){
+        document.getElementById("songs").innerHTML = " No results found" ;
+    }else{
+        for(var i = 0; i < numOfRe; i++){
+            var rank = i+1;
+            tableInfo += "<tr>"+"<td>"+"<br>"+ rank + "<br></td>";
+            tableInfo  += "<td>"+"<br>" + "<img src = " + myData.results[i].artworkUrl100
+                +">"+"<br>" + "</td>";
+            tableInfo += "<td>\n" +
+                "<a href=\"detailpage.html?song=" + i+ "&artist="+ myData.results[i].artistName+ "\"><button>info</button></a></td>";
+            tableInfo += "<td><br>Artist Name: " + myData.results[i].artistName+ "<br></td>";
+            tableInfo  += "<td>"+"<br>" + "Track Name: "+ myData.results[i].trackName+ "<br>" + "</td>";
+            tableInfo  += "<td>"+"<br>" + " Album: "+ myData.results[i].collectionName+ "<br>" + "</td>";
+            tableInfo  += "<td><audio controls='true' src=" + myData.results[i].previewUrl + " id='audio' type='audio/m4a'></audio>"+"</td></tr>";
+        }
 
 
+        document.getElementById("songs").innerHTML = tableInfo;
 
-    for(var i = 0; i < numOfRe; i++){
-        var rank = i+1;
-        tableInfo += "<tr>"+"<td>"+"<br>"+ rank + "<br></td>";
-        tableInfo  += "<td>"+"<br>" + "<img src = " + myData.results[i].artworkUrl100
-            +">"+"<br>" + "</td>";
-        tableInfo += "<td><br>Artist Name: " + myData.results[i].artistName+ "<br></td>";
-        tableInfo  += "<td>"+"<br>" + "Track Name: "+ myData.results[i].trackName+ "<br>" + "</td>";
-        tableInfo  += "<td>"+"<br>" + " Album: "+ myData.results[i].collectionName+ "<br>" + "</td>";
-        tableInfo  += "<td><audio controls='true' src=" + myData.results[i].previewUrl + " id='audio' type='audio/m4a'></audio>"+"</td></tr>";
     }
 
 
-    document.getElementById("songs").innerHTML = tableInfo;
-
 }
+
+function moreInfo(myData){
+    console.log(myData);
+    var numOfRe = document.getElementById('numberOfResults').value;
+
+    var info = "";
+
+    for(var i = 0; i < numOfRe; i++) {
+        info += "<td><br>Release Date: " + myData.results[i].releaseDate + "<br></td>";
+        info += "<td>" + "<br>" + "Track Duration: " + (myData.results[i].trackTimeMillis) / 60000
+            + "<br>" + "</td>";
+        info += "<td>" + "<br>" + " Explicitness: " + myData.results[i].trackExplicitness
+            + "<br>" + "</td>";
+    }
+    document.getElementById("songs").innerHTML = info;
+}
+
 function failed(){
-        document.getElementById("songs").innerHTML += "<br>" + alert(" No results found") ;
+        document.getElementById("songs").innerHTML = "Error" ;
 }
